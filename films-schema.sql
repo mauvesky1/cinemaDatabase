f53cea145839658504e87bebc87d5186cd7cb640
RobertDraper <rob.draper@live.com>
2021-08-19 10:50:24 +0100

    changing SQL

--- films-schema.sql
+++ films-schema.sql
@@ -6,8 +6,8 @@ CREATE TABLE films(
     film_id INT AUTO_INCREMENT,
     film_type VARCHAR(40) NOT NULL,
     price DECIMAL NOT NULL,
-    rating TINYINT,
-    screen_no. TINYINT NOT NULL,
+    rating INT,
+    screen_no. INT,
     PRIMARY KEY(film_id)
 );
 
