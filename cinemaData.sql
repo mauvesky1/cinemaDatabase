b176367ab0cface7d5279184ce708e39d97f2aab
RobertDraper <rob.draper@live.com>
2021-08-19 14:51:20 +0100

    changing schema

--- cinemaData.sql
+++ cinemaData.sql
@@ -1,8 +1,7 @@
 INSERT INTO films(
-film_type, price, rating, screen_no
-)
-VALUES('Horror', 5.2, 2, 2), 
-('Sci-fi', 5.2, 4, 2,),
-('Romance', 5.2, 1, 1);
+film_type, price, rating, screen_no)
+VALUES('Horror', 5, 2, 2),
+('Sci-fi', 5, 4, 2,),
+('Romance', 5, 1, 1);
 
 
--- schema/films-schema.sql
+++ schema/films-schema.sql
@@ -5,7 +5,7 @@ USE films;
 CREATE TABLE films(
     film_id INT AUTO_INCREMENT,
     film_type VARCHAR(40) NOT NULL,
-    price DECIMAL NOT NULL,
+    price INT,
     rating INT,
     screen_no INT,
     PRIMARY KEY(film_id)
