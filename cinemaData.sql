d41e0ca1793bfc81fed40b80ee4f27da89919a62
RobertDraper <rob.draper@live.com>
2021-08-19 14:35:38 +0100

    changed price

--- cinemaData.sql
+++ cinemaData.sql
@@ -1,8 +1,8 @@
 INSERT INTO films(
 film_type, price, rating, screen_no
 )
-VALUES("Horror", 5.00, 2, 2), 
-("Sci-fi", 5.00, 4, 2,),
-("Romance", 5.00, 1, 1);
+VALUES("Horror", 5, 2, 2), 
+("Sci-fi", 5, 4, 2,),
+("Romance", 5, 1, 1);
 
 
