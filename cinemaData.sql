b09f08bc9291a352647b4095ae552ee5bdebf56c
RobertDraper <rob.draper@live.com>
2021-08-19 14:33:22 +0100

    again

--- cinemaData.sql
+++ cinemaData.sql
@@ -5,13 +5,4 @@ VALUES("Horror", 5.00, 2, 2),
 ("Sci-fi", 5.00, 4, 2,),
 ("Romance", 5.00, 1, 1);
 
-INSERT INTO customers(
-    customer_name, customer_address
-)
-VALUES("Bob", "742 EverGreen Terrace"),
-("Sally", "Caprica Drive"),
-("Angelika", "Pickles Lane")
 
--- at least 3 values to each table
---delete at least one value from each table (using where)
--- Delete from <table name where <thing>
