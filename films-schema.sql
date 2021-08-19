d9cac2f6bdc3a5e49d126f865922e59ac3bb7f4d
RobertDraper <rob.draper@live.com>
2021-08-19 10:52:50 +0100

    starting from the start

--- films-schema.sql
+++ films-schema.sql
@@ -7,26 +7,6 @@ CREATE TABLE films(
     film_type VARCHAR(40) NOT NULL,
     price DECIMAL NOT NULL,
     rating INT,
-    screen_no. INT,
+    screen_no INT,
     PRIMARY KEY(film_id)
 );
-
-CREATE TABLE customers(
- 
-    customer_id INT AUTO_INCREMENT,
-    customer_name VARCHAR(222) NOT NULL,
-    customer_address VARCHAR(222) NOT NULL,
-    new_customer boolean,
-    PRIMARY KEY(customer_id)
-);
-
-CREATE TABLE orders(
-	order_id INT AUTO_INCREMENT,
-	date_booking DATETIME NOT NULL,
-	fk_film_id INT,
-    fk_customer_id INT,
-	PRIMARY KEY(order_id),
-	FOREIGN KEY(fk_film_id) REFERENCES films(film_id),
-    FOREIGN KEY(fk_customer_id) REFERENCES customers(customer_id)
-);
-
