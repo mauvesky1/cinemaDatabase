17a0d5c8a9623b60e7bf29191617b9b12ec8b3aa
RobertDraper <rob.draper@live.com>
2021-08-19 15:01:44 +0100

    Adding more data to films

--- cinemaData.sql
+++ cinemaData.sql
@@ -1,5 +1,5 @@
 INSERT INTO films(
-film_type)
-VALUES('Horror');
+film_type, price, rating, screen_no)
+VALUES('Horror', 5.1, 2, 2);
 
 
--- schema/cinema-schema.sql
+++ schema/cinema-schema.sql
@@ -5,7 +5,7 @@ USE cinema;
 CREATE TABLE films(
     film_id INT AUTO_INCREMENT,
     film_type VARCHAR(40) NOT NULL,
-    price INT,
+    price DECIMAL(10,2),
     rating INT,
     screen_no INT,
     PRIMARY KEY(film_id)
