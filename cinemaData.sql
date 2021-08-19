eb7c5d6542540dfabeae5b3a33dbdfed9bd5b722
RobertDraper <rob.draper@live.com>
2021-08-19 15:04:04 +0100

    Adding data to two tables

--- cinemaData.sql
+++ cinemaData.sql
@@ -1,5 +1,12 @@
 INSERT INTO films(
 film_type, price, rating, screen_no)
-VALUES('Horror', 5.1, 2, 2);
+VALUES('Horror', 5.1, 2, 2),
+('Sci-fi', 5.2, 4, 2,),
+('Romance', 5.2, 1, 1);
 
-
+INSERT INTO customers(
+    customer_name, customer_address
+)
+VALUES("Bob", "742 EverGreen Terrace"),
+("Sally", "Caprica Drive"),
+("Angelika", "Pickles Lane")
